b9398a0e09a15e7066d1abfb4d7953258932fc9c
Jake <jwhite52@gmail.com>
2015-10-14 18:59:54 -0600

    about page nav-bar work check-in

---
+++ js/home-page.js
@@ -0,0 +1,16 @@
+// $('.headings-home').on('mouseover', function(){
+// 	$(this).css("font-size", "50px");
+// }, function(){
+// 	$(this).css("font-size", "30px");
+// })
+
+// $('.headings-home').hover(function(){
+// 	$(this).stop().animate({ fontSize : '50px' });
+// },
+// function() {
+// 	$(this).stop().animate({ fontSize : '30px' });
+// })
+
+// $('ul.nav li a').each(function(){
+//     $(this).parent().width($(this).width() + 4);
+// });
